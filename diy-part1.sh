eb157b3cca33ac2d4bf0831f7d91d743c6ad8992
Shane <shanefengcn@hotmail.com>
2022-02-08 19:03:51 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,4 +15,4 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git luci-theme-argon https://github.com/jerrykuku/luci-theme-argon' >>feeds.conf.default
+git clone https://github.com/jerrykuku/luci-theme-argon package
